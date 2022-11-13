06fc691bb9a13e648805f6dcdc5360f13e63a9c1
Julia Fernandes <85169680+juh-rawn@users.noreply.github.com>
2022-11-13 20:00:43 -0300

    adição do arquivo styles.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import {} from 'react-icons/fi'
+import './styles.css';
 
 function App() {
   return (
     <div className="container">
-      <h1 className="titulo">PagBank</h1>
+      <h1 className="titulo">PagFast</h1>
       <button className="loginbtn">Login</button>
       <button className="cadastrarbtn">Cadastrar</button>
     </div>
