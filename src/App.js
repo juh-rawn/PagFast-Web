1493d4fa80a3db3d9b6287b3c32ee76e7dafefa2
Julia Fernandes <85169680+juh-rawn@users.noreply.github.com>
2022-11-13 19:30:47 -0300

    criação projeto PagFast

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import {} from 'react-icons/fi'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="titulo">PagBank</h1>
+      <button className="loginbtn">Login</button>
+      <button className="cadastrarbtn">Cadastrar</button>
     </div>
   );
 }
