b366f8b09fbdaa8b4012dfd2f6bf9233f1f44a3c
Julia Fernandes <85169680+juh-rawn@users.noreply.github.com>
2022-11-13 22:43:55 -0300

    renomeando nomes de arquivos e sistemas de pasta

---
+++ src/tela-login/login.js
@@ -0,0 +1,16 @@
+//import {} from 'react-icons/fi'
+import './login.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className='tela'>
+        <h1 className="titulo">PagFast</h1>
+        <button className="loginbtn">Login</button>
+        <button className="cadastrarbtn">Cadastrar</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
