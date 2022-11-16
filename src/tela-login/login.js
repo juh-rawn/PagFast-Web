5b9207d694f3c9a1348af6120260006e37a23316
Julia Fernandes <85169680+juh-rawn@users.noreply.github.com>
2022-11-16 19:07:40 -0300

    tela de login

--- src/tela-login/login.js
+++ src/tela-login/login.js
@@ -1,16 +1,102 @@
 //import {} from 'react-icons/fi'
+import React from 'react';
+import { View, KeyboardAvoidingView, Image, StyleSheet, Text, TextInput, TouchableOpacity, } from 'react-native';
+//import { View, KeyboardAvoidingView, Image } from 'react-native';
 import './login.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="container">
-      <div className='tela'>
-        <h1 className="titulo">PagFast</h1>
-        <button className="loginbtn">Login</button>
-        <button className="cadastrarbtn">Cadastrar</button>
-      </div>
-    </div>
-  );
+    <KeyboardAvoidingView style={styles.background}>
+        <View style={styles.containerLogo}>
+            <Text>PagFast</Text>
+            <img src={require('./logo.jpg')} alt='logo'/>
+        </View>
+        <View style={styles.container}>
+          <TextInput
+          style={styles.input}
+          placeholder="000.000.000/00"
+          autoCorrect={false}
+          onChangeText={()=> {}}
+          />
+
+          <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          autoCorrect={false}
+          onChangeText={()=> {}}
+          />
+
+          <TouchableOpacity style={styles.btnSubmit}>
+            <Text style={styles.submitText}>Entrar</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.btnRegister}>
+            <Text style={styles.registerText}>Cadastrar</Text>
+          </TouchableOpacity>
+
+        </View>
+      </KeyboardAvoidingView>
+    );
 }
 
-export default App;
+const styles = StyleSheet.create({
+    background:{
+      flex:1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#03ffff',
+    },
+
+    containerLogo:{
+      flex:1,
+      justifyContent:'center',
+      backgroundColor:'red',
+    },
+
+    container:{
+      flex:1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '50%',
+      paddingBottom: 50,
+    },
+
+    input:{
+      backgroundColor:'#D9D9D9',
+      width: '40%',
+      marginBottom: 15,
+      color: '#222',
+      fontSize: 17,
+      borderRadius: 10,
+      padding: 10,
+    },
+
+    btnSubmit:{
+      backgroundColor: '#ccc',
+      width: '10%',
+      height: '10%',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius:10,
+    },
+
+    submitText:{
+      fontSize: 18
+    },
+
+    btnRegister:{
+      backgroundColor: '#ccc',
+      width: '15%',
+      height: '10%',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius:10,
+      marginTop:10,
+    },
+
+    registerText:{
+      fontSize: 18
+    }
+})
+
+ 
