99cf7c958bcadb108c5764c26689a45bd5b503c5
Julia Fernandes <85169680+juh-rawn@users.noreply.github.com>
2022-11-20 14:21:12 -0300

    ajustes css home e login page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,16 +13,60 @@ const HomePage = () => {
     }
 
     return (
-        <div id="HomePage">
-            <h1 className="title"> PagFast</h1>
-            <img src={require('./../assets/logo.png')} alt="logo PagFast"/>
-            <form onSubmit={handleSubmit}>
-                <div className="homeBtn">
-                    <button type="submit">Login</button> 
-                    {/* fazer validação de mascara e placeholder */}
-                    <button type="submit">Cadastrar</button>
-                </div>
-            </form>
+        <div id="home">
+            {/* <h1 className="title"> PagFast</h1> */}
+            <div className="img"><img src={require('./../assets/logo.png')} alt="logo PagFast" 
+            style={{
+                paddingBottom: "40px",
+            }}/></div>
+            <div id="container">
+                <form onSubmit={handleSubmit} style={{
+                    // backgroundColor: "#d9d9d9",
+                    width: "100%",
+                    maxWidth: "480px",
+                    padding: "0.5rem",
+                }}>
+                    <button type="submit" style={{
+                        backgroundColor:"#D9D9D9",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        borderRadius: "5px",
+                        marginBottom: "30px",
+                        fontSize: "30px",
+                        fontWeight: "bold",
+                        width: "100%",
+                        padding: "0.5rem",
+                        textAlign: "center",
+                        marginTop: "0.5rem",
+                        border:"none",
+
+                        boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+                    
+
+                    }}
+                        >Login</button> 
+                    <button type="submit" disabled style={{
+                        backgroundColor:"#D9D9D9",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        borderRadius: "5px",
+                        fontSize: "30px",
+                        fontWeight: "bold",
+                        width: "100%",
+                        padding: "0.5rem",
+                        textAlign: "center",
+                        marginTop: "0.5rem",
+                        border:"none",
+
+                        boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+                       
+                    }}>Cadastrar</button>
+                </form>
+            </div>
         </div>
     );
 };
--- src/pages/tela-login/loginApp.jsx
+++ src/pages/tela-login/loginApp.jsx
@@ -11,37 +11,76 @@ const LoginPage = () => {
     const [cpf, setCpf] = useState("");
     const [password, setPassword] = useState("");
 
-const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("submit", { cpf, password});
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("submit", { cpf, password});
 
     login(cpf, password);// integração com o contexto / api
-};
+    };
 
     return (
         <div id="login">
-            <h1 className="title"> Login do sistema</h1>
+            {/* <h1 className="title"> Login do sistema</h1> */}
             <img src={require('./../../assets/logo.png')} alt="logo PagFast"/>
-            <p>{String(authenticated)}</p>
-            <form className="form" onSubmit={handleSubmit}>
+            <div className="auth" style={{
+                display:"none",
+            }}>
+                <p>{String(authenticated)} </p>
+            </div>
+            
+            <form className="form" onSubmit={handleSubmit}
+                style={{
+                    borderRadius: "9px",
+                    padding: "0.6rem",
+                }}
+            >
+                
                 <div className="field">
                     <label htmlFor="number">CPF</label> 
                     {/* fazer validação de mascara e placeholder */}
                     <input type="text" name="cpf" 
-                    pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" 
-                    title="000.000.000-00"
-                    value={cpf}
-                    onChange={(e) => setCpf(e.target.value)}
+                        maxLength={14}
+                        pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" 
+                        title="000.000.000-00"
+                        placeholder="000.000.000-00"
+                        value={cpf}
+                        onChange={(e) => setCpf(e.target.value)}  
+                        style={{
+                            borderRadius: "5px", 
+                            border: "none", 
+                            padding: "0.3rem",
+                            textAlign: "left",
+                            marginTop: "0.5rem",
+                            justifyContent: "center",
+                        }}
                     />
                 </div>
+        
                 <div className="field">
                     <label htmlFor="password">Senha</label>
                     <input type="password" name="password" id="password" 
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}/>
+                        value={password}
+                        placeholder="******"
+                        onChange={(e) => setPassword(e.target.value)}
+                        style={{
+                            borderRadius: "5px", 
+                            border: "none", 
+                            padding: "0.3rem",
+                            textAlign: "left",
+                            marginTop: "0.5rem",
+                            justifyContent: "center",
+                        }}
+                    />
                 </div>
                 <div className="actions">
-                    <button type="submit">Entrar</button>
+                    <button type="submit" 
+                    style={{
+                        borderRadius: "5px", 
+                        border: "none", 
+                        padding: "0.5rem",
+                        boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+                    }}
+                    >Entrar</button>
                 </div>
             </form>
         </div>
